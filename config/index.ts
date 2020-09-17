f0e440deb09e9f6572a7cea305013435d0ca3a6c
avi tshuva <dharmax@gmail.com>
2020-09-17 19:13:11 +0300

    cont...

---
+++ config/index.ts
@@ -0,0 +1,7 @@
+// created from 'create-ts-index'
+
+export * from './app-config';
+export * from './deployment';
+export * from './docker-support';
+export * from './run-mode';
+export * from './server-address';
